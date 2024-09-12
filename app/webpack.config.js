906f291e613197ff6ab09e984c635ea08aba2d20
Jelle Roets <jelle.roets@arcnc.com>
2024-09-12 14:27:41 +0200

    init repo

---
+++ app/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true
+  }
+};
